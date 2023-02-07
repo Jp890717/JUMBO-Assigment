1f0b8dd13e7d2b258db8861cae370de72e05a775
Jp Engelbrecht <juan-pierre.engelbrecht@investec.co.za>
2023-02-07 08:39:00 +0200

    Updates to city store component

--- jumbo/src/app/components/city-stores/city-stores.component.spec.ts
+++ jumbo/src/app/components/city-stores/city-stores.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CityStoresComponent } from './city-stores.component';
+import {NgxsModule, Store} from "@ngxs/store";
+import {Router} from "@angular/router";
+import {RouterTestingModule} from "@angular/router/testing";
+import {AddCity, StoreState} from "../../state/stores.state";
 
 describe('CityStoresComponent', () => {
   let component: CityStoresComponent;
   let fixture: ComponentFixture<CityStoresComponent>;
+  let store: Store;
+  let router: Router;
+  let cityName: string;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CityStoresComponent ]
+      imports: [
+        RouterTestingModule,
+        NgxsModule.forRoot([StoreState])
+      ],
+      declarations: [ CityStoresComponent ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CityStoresComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- jumbo/src/app/components/city-stores/city-stores.component.ts
+++ jumbo/src/app/components/city-stores/city-stores.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Store} from "@ngxs/store";
 import {StoreState} from "../../state/stores.state";
 import {StoreList} from "../../models/stores";
+import {NavigationExtras, Router} from "@angular/router";
 
 @Component({
   selector: 'app-city-stores',
@@ -13,14 +14,25 @@ export class CityStoresComponent implements OnInit {
   cityName: string = '';
   stores: StoreList[] =[];
 
-  constructor(private store:Store) {
-    this.store.select(StoreState.getCity).subscribe(city => this.cityName = city);
+  constructor(private store:Store, private router: Router) {
+
   }
 
   ngOnInit(): void {
+    this.store.select(StoreState.getCity).subscribe(city => this.cityName = city);
+
     this.store.select(StoreState.getStores).subscribe((stores:StoreList[]) => {
       this.stores = stores.filter(item => item.city === this.cityName);
     })
+  }
+
+  back() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        cities: true
+      }
+    };
+    this.router.navigate(['/stores'], navigationExtras)
   }
 
 }
