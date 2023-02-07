056f122acba8287ef63e5da209a73c7e55ff9f1e
Jp Engelbrecht <juan-pierre.engelbrecht@investec.co.za>
2023-02-07 08:38:05 +0200

    Added secondary button and unit test

--- jumbo/src/app/components/button/button.component.spec.ts
+++ jumbo/src/app/components/button/button.component.spec.ts
@@ -5,6 +5,8 @@ import { ButtonComponent } from './button.component';
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  const btnClass = 'btn-primary';
+  const buttonText = 'button';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +16,20 @@ describe('ButtonComponent', () => {
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    component.btnClass = btnClass;
+    component.buttonText = buttonText;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set btnClass input property', () => {
+    expect(component.btnClass).toEqual(btnClass);
+  });
+
+  it('should set buttonText input property', () => {
+    expect(component.buttonText).toEqual(buttonText);
   });
 });
